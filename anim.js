642dad637145cbfc6cb7ec7ef0e490d056cba6d9
Aayush S C <78192713+animemandir@users.noreply.github.com>
2021-10-28 19:21:29 +0530

    Create anim.js

---
+++ anim.js
@@ -0,0 +1,4 @@
+function titleFunction(title) {
+document.getElementById(&quot;videoTitle&quot;).innerText = title.getAttribute(&quot;vidtitle&quot;);}
+function go(loc) {
+document.getElementById(&#39;anim-player&#39;).src = loc;}
